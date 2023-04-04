9ab42d3b3bc4ed2353b38cfc17f8af2f4346a768
balazsmogyorodi <balazsmogyorodi@gmail.com>
2023-04-04 23:15:02 +0200

    remove elements

--- lista.js
+++ lista.js
@@ -1,7 +1,14 @@
+
+
+
+
+
 export const OBJEKTUMLISTA =[
-    { nev: "Dézi", fajta: "keverék", kor:2},
-    { nev: "Bodri", fajta: "mudi", kor:13},
-    { nev: "Loki", fajta: "tacskó", kor:5},
-    { nev: "Balint", fajta: "foxterrier", kor:4},
-    { nev: "Happy", fajta: "németjuhász", kor:42}
+    { nev: "Dézi", fajta: "keverék", kor:2, id: 0},
+    { nev: "Bodri", fajta: "mudi", kor:13, id: 1},
+    { nev: "Loki", fajta: "tacskó", kor:5, id: 2},
+    { nev: "Balint", fajta: "foxterrier", kor:4, id: 3},
+    { nev: "Happy", fajta: "németjuhász", kor:42, id: 4}
 ]
+
+
--- main.js
+++ main.js
@@ -1,69 +1,74 @@
-import{OBJEKTUMLISTA} from "./lista.js";
-import {rendezesObjektum} from "./rendezes.js";
+import { OBJEKTUMLISTA } from "./lista.js";
+import { rendezesObjektum } from "./rendezes.js";
+const kuka = `<img src="képek/kuka.png" alt="tölés" class="kuka"`;
+$(function () {
+  init();
+});
 
-const kuka = `<img src="képek/kuka.png" alt="tölés" class="kuka"`
-$(function(){
-    init();
-
-
-  });
-
-
-function init(){
+function init() {
   console.log("hi");
   const articleElem = $("article");
   let tablazat = tablazatKeszit(OBJEKTUMLISTA);
   console.log(tablazat);
   articleElem.html(tablazat);
   const thElem = $("th");
-  thElem.on("click", function(){
-    let kulcs = $(event.target).attr("id")
+  thElem.on("click", function () {
+    let kulcs = $(event.target).attr("id");
     rendezesObjektum(OBJEKTUMLISTA, kulcs);
     console.log(OBJEKTUMLISTA);
     init();
   });
-  const kukaIcon = $(".kuka");
-  kukaIcon.on("click", function(){
-    const kuka = $(this);
-    const sor = kuka.attr("id");
-    torles(sor);
+  kukazas();
 
-
-    console.log(sor);
-
-
-    
- 
-
-
-
-  });
-
-
-  
 
 }
-function tablazatKeszit(OBJEKTUMLISTA){
-    let tablazat = "<table class='table table-striped'>";
-    tablazat += " <thead class='table-dark'>";
-    tablazat += "<tr> <th id='nev'> Név: </th> <th id='kor'>Kor:</th><th id='fajta'> Fajta: </th> <th> </th></tr>"
-    tablazat += "</thead>" 
-    for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
-      tablazat +="<tr>";
-      tablazat += "<td>" + OBJEKTUMLISTA[index].nev + "</td>" + "<td>" + OBJEKTUMLISTA[index].kor +"</td>" + "<td>" + OBJEKTUMLISTA[index].fajta +"</td>" + "<td>" + kuka + `id="${index}">` + "</td>";
-      tablazat +="</tr>";
-    }
-    tablazat += "</table>";
-    
-    return tablazat += "</table>";
-  
-  
+  function kukazas(){
+  const kukaIcon = $(".kuka");
+  kukaIcon.on("click", function () {
+    const kuka = $(this);
+    const sor = kuka.attr("id");
+   torles(sor);
+  });
+}
+  function torles(sor) {
+      $(document).ready(function(){
+      $("table").on('click', '.kuka',function(){
+          $(this).closest('tr').remove();
+          console.log(OBJEKTUMLISTA[sor].nev);
+          delete OBJEKTUMLISTA[sor];
+         
+      });
+    });
+}
+
+
+
+
+function tablazatKeszit(OBJEKTUMLISTA) {
+  let tablazat = "<table class='table table-striped'>";
+  tablazat += " <thead class='table-dark'>";
+  tablazat +=
+    "<tr id ='semmi'> <th id='nev'> Név: </th> <th id='kor'>Kor:</th><th id='fajta'> Fajta: </th> <th> </th></tr>";
+  tablazat += "</thead>";
+  for (let index = 0; index < OBJEKTUMLISTA.length; index++) {
+    tablazat += `<tr id="${index}">`;
+    tablazat +=
+      "<td>" +
+      OBJEKTUMLISTA[index].nev +
+      "</td>" +
+      "<td>" +
+      OBJEKTUMLISTA[index].kor +
+      "</td>" +
+      "<td>" +
+      OBJEKTUMLISTA[index].fajta +
+      "</td>" +
+      "<td>" +
+      kuka +
+      `id="${OBJEKTUMLISTA[index].id}">` +
+      "</td>";
+    tablazat += "</tr>";
   }
+  tablazat += "</table>";
 
-
-  function torles() {
-
-
-    
-  }
-  
+  return (tablazat += "</table>");
+}
